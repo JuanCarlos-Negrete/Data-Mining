2172968d516601c8fc74abaa9568d0eea0893655
ManuelSifuentes <manuel.sifuentes17@tectijuana.edu.mx>
2021-05-28 20:34:57 -0700

    first part of practice 02

--- Unit_3/Practices/Practice02/Practice01.R
+++ Unit_3/Practices/Practice02/Practice01.R
@@ -1,63 +1,31 @@
-# We add the library that we will work with
-library(ggplot2)
+# K-Nearest Neighbors (K-NN)
 
-# We access the path where the files are located
-getwd()
-setwd("C:/Users/jc_rc/Data Mining Class/DataMining/AdvancedVisualization")
-getwd()
+# Importing the dataset
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[3:5]
 
-# We load our CSV with the variable "music"
-music <- read.csv("SpotifyFeatures.csv")
+# Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
 
-# We apply the statistical functions to observe the data
-head(music)
-tail(music)
-str(music)
-summary(music)
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
 
-#----- Point scatter plot -----#
+# Feature Scaling
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
 
-# We assign to the variable newgraph the columns "duration_ms" and "popularity" of the CSV for X and Y
-newgraph <- ggplot(music, aes(x=duration_ms, y=popularity, 
-                       color=ï..genre)) 
+# Fitting K-NN to the Training set and Predicting the Test set results
+library(class)
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5,
+             prob = TRUE)
 
-# The point plot is created
-newgraph + geom_point() + xlab("Duration (ms)") + ylab("Popularity")
-
-#----- Graph in facets -----#
-
-#Load V-chart
-v <- ggplot(music, aes(x=popularity))
-
-#Facets
-v + geom_histogram(binwidth = 10, aes(fill=ï..genre),
-                   color="Black") + facet_grid(ï..genre~., scales="free")
-
-
-#-------------- Histograms and density graphs
-o <- ggplot(music, aes(x=popularity))
-
-
-# Density charts:
-h <- o + geom_density(aes(fill=ï..genre), position = "stack")
-
-#Histograms
-h <- o + geom_histogram(binwidth = 10, aes(fill=ï..genre), color="Black")
-
-
-# Theme #
-h +
-  xlab("Popularity") +
-  ylab("Number of Songs") +
-  ggtitle("Music Popularity Distribution") +
-  theme(axis.title.x = element_text(color = "DarkBlue", size=25),
-        axis.title.y = element_text(color = "DarkRed", size=25),
-        axis.text.x = element_text(size = 15),
-        axis.text.y = element_text(size = 15),
-        legend.title = element_text(size = 20),
-        legend.text = element_text(size = 15),
-        legend.position = c(1,1),
-        legend.justification = c(1,1),
-        plot.title = element_text(color = "Black",
-                                  size = 30,
-                                  family = "Courier"))
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
